ae2e28efee9a8215e7f8bba3de6115a4eb560287
Aaronphilip2003 <aaronphilip2003@gmail.com>
2025-08-22 09:16:03 +0530

    worked on signalling

--- routers/signaling.py
+++ routers/signaling.py
@@ -1,8 +1,39 @@
-# routers/signaling.py
-from fastapi import APIRouter
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+from typing import Dict, List
 
 router = APIRouter()
 
-@router.get("/ping")
-async def ping():
-    return {"message": "pong"}
+# Store active WebSocket connections per room
+active_connections: Dict[str, List[WebSocket]] = {}
+
+@router.websocket("/{room_id}")
+async def signaling_endpoint(websocket: WebSocket, room_id: str):
+    await websocket.accept()
+
+    # Add client to room
+    if room_id not in active_connections:
+        active_connections[room_id] = []
+    active_connections[room_id].append(websocket)
+
+    try:
+        while True:
+            message = await websocket.receive()
+
+            if "text" in message:
+                data = message["text"]
+            elif "bytes" in message:
+                data = message["bytes"]
+            else:
+                data = message
+
+            # Relay to all other participants
+            for conn in active_connections[room_id]:
+                if conn is not websocket:
+                    await conn.send_text(str(data))
+
+
+    except WebSocketDisconnect:
+        # Remove disconnected client
+        active_connections[room_id].remove(websocket)
+        if not active_connections[room_id]:
+            del active_connections[room_id]
